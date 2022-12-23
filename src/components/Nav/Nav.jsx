e5ed7fd4c594ac7444863c58e527cc123a8826cb
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-12-22 20:11:54 -0600

    Updated Logo in Nav and added initial CSS styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,15 +3,43 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+
+// material-UI components
+// import { makeStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//       flexGrow: 1,
+//   },
+//   menuButton: {
+//       marginRight: theme.spacing(2),
+//   },
+//   title: {
+//       flexGrow: 1,
+//   },
+// }));
 
 function Nav() {
   const user = useSelector((store) => store.user);
+  // const classes = useStyles();
+  const history = useHistory();
+
+  const navHome = () => {
+    history.push('/')
+}
 
   return (
+    // <div className={classes.root}>
     <div className="nav">
-      <Link to="/home">
-        <img src='images/PACK HACK Inverted Color 2000x1500.png' />
-      </Link>
+      {/* <AppBar position="static">
+        <Toolbar>
+          <button onClick={navHome}>Home</button>
+        </Toolbar>
+      </AppBar> */}
+        <img style={{ width: 200, height: 150 }} src="/images/PACKHACKInvertedColor2000x1500.png" />
       <div>
         {/* If no user is logged in, show these links */}
         {!user.id && (
@@ -27,7 +55,7 @@ function Nav() {
             <Link className="navLink" to="/user">
               Home
             </Link>
-            
+
             <LogOutButton className="navLink" />
           </>
         )}
