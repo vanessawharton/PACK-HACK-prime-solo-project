b4d9000ab8e8524be797e8da255d6b58c1c3bbff
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-01-12 15:28:37 -0600

    Added inline editing for items

--- src/components/Items/ItemView.jsx
+++ src/components/Items/ItemView.jsx
@@ -1,19 +1,22 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 // styling
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import InlineEdit from '../Items/InlineEdit';
 
 
 
 function ItemView( {item} ) {
     const dispatch= useDispatch();
+
+    const [value, setValue] = useState();
 
     const handleToggle = () => {
         dispatch({ type: 'PACK_ITEM', payload: item.id})
@@ -22,6 +25,7 @@ function ItemView( {item} ) {
     const handleDelete = () => {
         dispatch({ type: 'DELETE_ITEM', payload: item.id});
     };
+
 
     return (
         <center>
@@ -39,7 +43,7 @@ function ItemView( {item} ) {
                 }
             >
                 <ListItemButton>
-                    <ListItemText id={item.id} primary={item.name} />
+                    <InlineEdit value={item.name} setValue={setValue} />
                 </ListItemButton>
                 <IconButton aria-label="delete"
                     onClick={handleDelete}
